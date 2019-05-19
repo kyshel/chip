9462a7818ebe01463d615219a4fb4993fb6c9ba0
kyshel <1069072177@qq.com>
2019-05-19 22:05:03 +0800

    Update util.py

--- util.py
+++ util.py
@@ -2,6 +2,34 @@
 
 
 
+# ping   (unix windows OK)
+import platform    # For getting the operating system name
+import subprocess  # For executing a shell command
+import os
+
+def ping(host):
+	"""
+	Returns True if host (str) responds to a ping request.
+	Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
+	"""
+	try:
+	 	TIMEOUT_PING
+	except NameError:
+	 	TIMEOUT_PING = 2
+
+	timeout = TIMEOUT_PING * 1000 if platform.system().lower()=='windows' else  TIMEOUT_PING
+	number = '-n' if platform.system().lower() == 'windows' else '-c'
+	devnull = open(os.devnull, 'w')
+
+
+	# Building the command. Ex: "ping -c 1 google.com"
+	command = ['ping', number, '1', '-w', str(timeout) , host]
+
+	return subprocess.call(command,stdout=devnull, stderr=subprocess.STDOUT) == 0
+
+a = ping('8.8.8.8')
+
+print(a)
 
 
 
